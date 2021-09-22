51f392030bb8d9c1876bace04445c1e70f4d467a
Sutadasuto <rodrigo.rill@hotmail.com>
2021-09-22 22:10:57 +0200

    Code cleaning

--- run_experiments.py
+++ run_experiments.py
@@ -1,8 +1,4 @@
 import os
-import subprocess
-
-import matplotlib.pyplot as plt
-import numpy as np
 
 from data import generate_datasets, generate_dataset_summary, generate_plots, train_synthetic_model, \
     create_syncthetic_training_summary, train_real_model
